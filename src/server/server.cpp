822cadb50582140e24ded28ee5e1a77abe2a8cbd
Dmitry Ledentsov <dmlled@yahoo.com>
2015-08-06 01:13:26 +0200

    server listens to pushes

--- src/server/server.cpp
+++ src/server/server.cpp
@@ -1,37 +1,45 @@
 #include <iostream>
-
+#include <zmq.hpp>
 #include <rxcpp/rx.hpp>
 
 int main() {
+
+	zmq::context_t context(1);
+
+	// socket config
+	std::string port="5555";
+	std::string socket_config="tcp://*:";
+	socket_config+=port;
+
+	zmq::socket_t pull_from_workers(context, ZMQ_PULL);
+	pull_from_workers.bind(socket_config.c_str());
+
+	std::cout<<"Starting listening at "<<socket_config<<std::endl;
+
+	bool to_kill=false;
+	
 	// publish input from cin from a single input thread
-	// cin does not support multithreaded access
-	auto console_lines = rxcpp::observable<>::
+	auto worker_pings = rxcpp::observable<>::
 	    create<std::string>(
-	        [](rxcpp::subscriber<std::string> out){
-	            std::string line;
-	            while (std::getline(std::cin, line) && !line.empty()) {
-	                out.on_next(line);
-	            }
+	        [&pull_from_workers,&to_kill](rxcpp::subscriber<std::string> out){
+	            while (!to_kill) {
+					zmq::message_t request;
+					pull_from_workers.recv(&request);
+					out.on_next(std::string(static_cast<char*>(request.data()),request.size()));
+				}
 	            out.on_completed();
 	        }).
 	    as_dynamic().
 	    subscribe_on(rxcpp::synchronize_new_thread()).
 	    publish();
 
-	// non-blocking subscription 1
-	console_lines.subscribe([](std::string const& s) {
-	    // on input thread
-	    std::cout << "1:" << s << std::endl;
-	});
-
-	// non-blocking subscription 2
-	console_lines.subscribe([](std::string const& s) {
-	    // on input thread
-	    std::cout << "2:" << s << std::endl;
+	// non-blocking subscription
+	worker_pings.subscribe([](std::string const& s) {
+	    std::cout << "worker says:" << s << std::endl;
 	});
 
 	// start loop on input thread and block until complete
-	console_lines.
+	worker_pings.
 	    connect_forever().
 	    as_blocking().
 	    subscribe();
